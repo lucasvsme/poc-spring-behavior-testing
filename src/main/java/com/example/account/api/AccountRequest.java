ad46ec0382425177a3f05eb24ee21fe156f212dd
Lucas <lucasvalenteds@gmail.com>
2022-07-13 00:00:38 -0300

    Changing Account name regex
    Allowing any sequence of letters instead of only one letter.

--- src/main/java/com/example/account/api/AccountRequest.java
+++ src/main/java/com/example/account/api/AccountRequest.java
@@ -3,13 +3,15 @@ package com.example.account.api;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
 @Data
+@NoArgsConstructor
 public final class AccountRequest {
 
     @NotBlank(message = "Account name is required")
     @Length(min = 1, max = 15, message = "Account name must be at most 15 characters long")
-    @Pattern(regexp = "[a-zA-Z]", message = "Account name must contain letters only")
+    @Pattern(regexp = "[A-Za-z]*$", message = "Account name must contain letters only")
     private String name;
 }
