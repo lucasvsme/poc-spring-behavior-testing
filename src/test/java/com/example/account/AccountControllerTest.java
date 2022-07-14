5c21dacf91f0a718c7d0df01df287d748fb67b36
Lucas <lucasvalenteds@gmail.com>
2022-07-13 23:01:44 -0300

    Implementing automated tests for edge cases

---
+++ src/test/java/com/example/account/AccountControllerTest.java
@@ -0,0 +1,130 @@
+package com.example.account;
+
+import com.example.account.api.AccountRequest;
+import com.example.account.api.AccountResponse;
+import com.example.account.api.DepositRequest;
+import com.example.account.api.TransferRequest;
+import com.example.testing.AutoConfigureDatabaseContainer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.reactive.server.ExchangeResult;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.math.BigDecimal;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+@AutoConfigureDatabaseContainer
+class AccountControllerTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    void findingUnknownAccount() {
+        // Defining unknown ID to represent source account
+        final var accountId = 5L;
+
+        // Trying to find it and expecting an error
+        webTestClient.get()
+                .uri("/accounts/{accountId}", accountId)
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.NOT_FOUND)
+                .expectBody().isEmpty();
+    }
+
+    @Test
+    void depositingToUnknownAccount() {
+        // Defining unknown ID to represent source account
+        final var accountId = 5L;
+
+        final var depositRequest = new DepositRequest();
+        depositRequest.setAmount(BigDecimal.ONE);
+
+        webTestClient.post()
+                .uri("/accounts/{accountId}/deposit", accountId)
+                .bodyValue(depositRequest)
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.NOT_FOUND)
+                .expectBody().isEmpty();
+    }
+
+    @Test
+    void transferringFromUnknownSourceAccount() {
+        // Defining unknown ID to represent source account
+        final var sourceAccountId = 5L;
+
+        // Creating target account
+        final var targetAccountRequest = new AccountRequest();
+        targetAccountRequest.setName("A");
+        final var targetAccount = createAccount(targetAccountRequest);
+
+        // Defining the transfer
+        final var transferRequest = new TransferRequest();
+        transferRequest.setAmount(BigDecimal.ONE);
+        transferRequest.setTargetAccountId(targetAccount.getId());
+
+        // Transferring and inspecting the response is an error
+        webTestClient.post()
+                .uri("/accounts/{accountId}/transfer", sourceAccountId)
+                .bodyValue(transferRequest)
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.NOT_FOUND)
+                .expectBody().isEmpty();
+    }
+
+    @Test
+    void transferringFromUnknownTargetAccount() {
+        // Creating source account
+        final var sourceAccountRequest = new AccountRequest();
+        sourceAccountRequest.setName("B");
+        final var sourceAccount = createAccount(sourceAccountRequest);
+
+        // Defining unknown ID to represent target account
+        final var targetAccountId = 5L;
+
+        // Defining the transfer
+        final var transferRequest = new TransferRequest();
+        transferRequest.setAmount(BigDecimal.ONE);
+        transferRequest.setTargetAccountId(targetAccountId);
+
+        // Transferring and inspecting the response is an error
+        webTestClient.post()
+                .uri("/accounts/{accountId}/transfer", sourceAccount.getId())
+                .bodyValue(transferRequest)
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.NOT_FOUND)
+                .expectBody().isEmpty();
+    }
+
+    private AccountResponse createAccount(AccountRequest accountRequest) {
+        final var exchange = webTestClient.post()
+                .uri("/accounts")
+                .bodyValue(accountRequest)
+                .exchange()
+                .returnResult(Void.class);
+
+        final var accountId = getAccountIdFromLocationHeader(exchange);
+
+        return webTestClient.get()
+                .uri("/accounts/{accountId}", accountId)
+                .exchange()
+                .returnResult(AccountResponse.class)
+                .getResponseBody()
+                .blockFirst();
+    }
+
+    private Long getAccountIdFromLocationHeader(ExchangeResult exchangeResult) {
+        final var responseHeaders = exchangeResult.getResponseHeaders();
+        final var location = responseHeaders.getLocation();
+        assert location != null;
+
+        final var segments = location.toString().split("/");
+        final var lastSegment = segments[segments.length - 1];
+
+        return Long.parseLong(lastSegment);
+    }
+}
