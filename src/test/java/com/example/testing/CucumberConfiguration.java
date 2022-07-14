fca8f644498ce87e7e0c450fdeb71dcf7e6b919e
Lucas <lucasvalenteds@gmail.com>
2022-07-13 23:13:18 -0300

    Configuring Cucumber HTML report generation

--- src/test/java/com/example/testing/CucumberConfiguration.java
+++ src/test/java/com/example/testing/CucumberConfiguration.java
@@ -6,10 +6,12 @@ import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("com/example")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.example")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "html:build/reports/tests/bdd/index.html")
 public class CucumberConfiguration {
 }
